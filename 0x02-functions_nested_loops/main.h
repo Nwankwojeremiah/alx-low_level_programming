c2733b8896f5d48059b36101e5575ee4d39b4296
Nwankwojeremiah <nwankwojere@gmail.com>
2022-12-14 17:57:48 -0800

    task 4

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-int _putchar(char c)
+int _putchar(char c);
+void print_alphabet(void);
+void print_print_x10(void);
+int _islower(int c);
+int _isalpha(int c);
 
 #endif
