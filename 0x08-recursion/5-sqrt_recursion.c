0fdb8342550e87747b229dad2484c1d8f93ff376
Nwankwojeremiah <nwankwojere@gmail.com>
2023-01-04 06:30:57 -0800

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -22,7 +22,7 @@ int helperFunction(int num, int pSqrt)
 }
 
 /**
-  *_sqrt_recursion - returns the nutural square root a number.
+  *_sqrt_recursion - returns the nutural square root of a number.
   *@n: number to find sqrt of.
   *
   *Return: squareroot of n.
