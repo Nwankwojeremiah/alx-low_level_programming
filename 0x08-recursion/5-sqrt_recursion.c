c7dfb70ee16e52a1da1e0ec830f7cb1cdd07af20
Nwankwojeremiah <nwankwojere@gmail.com>
2023-01-05 08:58:45 -0800

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -14,7 +14,7 @@ int helperFunction(int num, int pSqrt)
 	}
 	else
 	{
-		if ((pSqrt * pSqrt) > num )
+		if ((pSqrt * pSqrt) > num)
 			return (-1);
 		else
 			return (helperFunction(num, pSqrt + 1));
